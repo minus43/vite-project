bc3c3c2ad458e5f6b1903268bc23a96a025b5a1b
minus43 <minu425854@gmail.com>
2025-03-26 15:38:53 +0900

    2-03 Router

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+// import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  );
+}
+
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React Router DOM</h1>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/topics">Topics</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/topics' element={<Topics />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={'Not Found'} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
