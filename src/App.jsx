649da8bbf3e7b8724d74bd56b265fdddc65cbbdc
minus43 <minu425854@gmail.com>
2025-03-28 14:28:36 +0900

    4 context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,50 @@
-import styled from 'styled-components';
-import './App.css'
+import React, {createContext, useContext} from 'react';
+import "./style.css";
 
-const SimpleButton = styled.button`
-  color: white;
-  background-color: green;
-`;
+const themeDefault = { border: '10px solid red'};
+const ThemeContext = createContext(themeDefault);
 
-const LargeButton = styled(SimpleButton)`
-  font-size: 50px;
-`;
-
-const ReactButton = props => {
-  console.log('props',props);
-  return <button className={props.className}>{props.children}</button>
-}
-
-const ReactLargeButton = styled(ReactButton)`
-  font-size: 50px;
-`;
-
-const PrimaryButton = styled.button`
-  color: ${props => props.primary ? 'white' : 'black'};
-  background-color: ${props => props.primary ? 'blue' : 'gray'};
-`;
 
 function App() {
+  const theme = useContext(ThemeContext);
   return (
-    <div>
-     <SimpleButton>Simple</SimpleButton>
-     <LargeButton>Large</LargeButton>
-     <br />
-     <ReactButton>React</ReactButton>
-     <ReactLargeButton>React Large</ReactLargeButton>
-     <br />
-     <PrimaryButton>Normal</PrimaryButton>
-     <PrimaryButton primary>Primary</PrimaryButton>
+    <ThemeContext.Provider value={{border: '10px solid blue'}}>
+    <div className="root" style={theme}>
+      <h1>Hello World!</h1>
+      <Sub1/>
     </div>
+    </ThemeContext.Provider>
   );
 }
 
+function Sub1() {
+  const theme = useContext(ThemeContext);
+  return (
+    <ThemeContext.Provider value={{border: '10px solid green'}}>
+    <div style={theme}>
+      <h1>Sub1</h1>
+      <Sub2/>
+    </div>   
+    </ThemeContext.Provider>
+  )
+}
+
+function Sub2() {
+  const theme = useContext(ThemeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub2</h1>
+      <Sub3/>
+    </div>
+  )
+}
+
+function Sub3() {
+  const theme = useContext(ThemeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub3</h1>
+    </div>
+  )
+}
 export default App
