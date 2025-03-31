b8aeadaeaf070a214bca8225ea53a72e25d1fa3c
minus43 <minu425854@gmail.com>
2025-03-31 14:50:22 +0900

    7 redex-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,28 @@
-import { useState } from 'react';
-import './App.css';
-import { configureStore } from '@reduxjs/toolkit';
-import { createStore } from 'redux';
-import { Provider, useSelector, useDispatch, connect } from 'react-redux';
+import React from 'react';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import store from './store';
+import { up } from './counterSlice';
 
 
-
-function reducer(currentState, action) {
-  if (currentState === undefined) {
-    return {
-      number: 1,
-    };
-  }
-  const newState = { ...currentState };
-  if (action.type === 'PLUS') {
-    newState.number++;
-  }
-  return newState;
-}
-const store = createStore(reducer);
-
-function App() {
-
-  return (
-    <div id="container">
-      <h1>Root </h1>
-      <Provider store={store}>
-      <div id="grid">
-        <Left1></Left1>
-        <Right1></Right1>
-      </div>
-      </Provider>
-    </div>
-  );
-}
-
-function Left1(props) {
-  return (
-    <div>
-      <h1>Left1</h1>
-      <Left2></Left2>
-    </div>
-  );
-}
-
-function Left2(props) {
-  return (
-    <div>
-      <h1>Left2</h1>
-      <Left3></Left3>
-    </div>
-  );
-}
-
-function Left3(props) {
-  const number = useSelector((state) => state.number);
-  return (
-    <div>
-      <h1>Left3 : {number}</h1>
-    </div>
-  );
-}
-
-function Right1(props) {
-  return (
-    <div>
-      <h1>Right1</h1>
-      <Right2></Right2>
-    </div>
-  );
-}
-
-function Right2(props) {
-  return (
-    <div>
-      <h1>Right2</h1>
-      <Right3></Right3>
-    </div>
-  );
-}
-
-function Right3(props) {
+function Counter() {
   const dispatch = useDispatch();
+  const count = useSelector((state) => {
+    console.log(state);
+    return state.counter.value;
+  });
   return (
     <div>
-      <h1>Right3</h1>
-      <input
-        type="button"
-        value="+"
-        onClick={() => {
-          dispatch({type: 'PLUS'});
-        }}
-      ></input>
+      <button onClick={() => dispatch(up(2))}>+</button> {count}
     </div>
-  );
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
+  )
+}
